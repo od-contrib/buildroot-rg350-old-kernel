2c36059386a22604cf4b682ce3d2bbf4d2bf2902
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-04-05 03:49:36 +0100

    Replace sdl, sdl2, sdl2_*
    Replaced with versions from https://github.com/Tonyjih/RG350_buildroot,
    which are based on https://github.com/gcwnow/buildroot/
    
    Added a patch to SDL2 for modern libtool compat

--- package/sdl2_image/sdl2_image.mk
+++ package/sdl2_image/sdl2_image.mk
@@ -4,55 +4,35 @@
 #
 ################################################################################
 
-SDL2_IMAGE_VERSION = 2.0.5
+SDL2_IMAGE_VERSION = 2.0.2
 SDL2_IMAGE_SOURCE = SDL2_image-$(SDL2_IMAGE_VERSION).tar.gz
 SDL2_IMAGE_SITE = http://www.libsdl.org/projects/SDL_image/release
 SDL2_IMAGE_INSTALL_STAGING = YES
-SDL2_IMAGE_LICENSE = Zlib
+SDL2_IMAGE_LICENSE = zlib
 SDL2_IMAGE_LICENSE_FILES = COPYING.txt
 
-# Unconditionally enable support for image formats that don't require
-# any dependency.
-SDL2_IMAGE_CONF_OPTS = \
-	--disable-sdltest \
-	--enable-bmp \
-	--enable-gif \
-	--enable-lbm \
-	--enable-pcx \
-	--enable-pnm \
-	--enable-tga \
-	--enable-xcf \
-	--enable-xpm \
-	--enable-xv
+SDL2_IMAGE_CONF_OPTS = --with-sdl-prefix=$(STAGING_DIR)/usr \
+		--with-sdl-exec-prefix=$(STAGING_DIR)/usr \
+		--disable-sdltest \
+		--disable-static \
+		--enable-bmp=$(if $(BR2_PACKAGE_SDL2_IMAGE_BMP),yes,no) \
+		--enable-gif=$(if $(BR2_PACKAGE_SDL2_IMAGE_GIF),yes,no) \
+		--enable-jpg=$(if $(BR2_PACKAGE_SDL2_IMAGE_JPEG),yes,no) \
+		--enable-lbm=$(if $(BR2_PACKAGE_SDL2_IMAGE_LBM),yes,no) \
+		--enable-pcx=$(if $(BR2_PACKAGE_SDL2_IMAGE_PCX),yes,no) \
+		--enable-png=$(if $(BR2_PACKAGE_SDL2_IMAGE_PNG),yes,no) \
+		--enable-pnm=$(if $(BR2_PACKAGE_SDL2_IMAGE_PNM),yes,no) \
+		--enable-tga=$(if $(BR2_PACKAGE_SDL2_IMAGE_TARGA),yes,no) \
+		--enable-tif=$(if $(BR2_PACKAGE_SDL2_IMAGE_TIFF),yes,no) \
+		--enable-webp=$(if $(BR2_PACKAGE_SDL2_IMAGE_WEBP),yes,no) \
+		--enable-xcf=$(if $(BR2_PACKAGE_SDL2_IMAGE_XCF),yes,no) \
+		--enable-xpm=$(if $(BR2_PACKAGE_SDL2_IMAGE_XPM),yes,no) \
+		--enable-xv=$(if $(BR2_PACKAGE_SDL2_IMAGE_XV),yes,no)
 
-SDL2_IMAGE_DEPENDENCIES = sdl2 host-pkgconf
-
-ifeq ($(BR2_PACKAGE_JPEG),y)
-SDL2_IMAGE_CONF_OPTS += --enable-jpg
-SDL2_IMAGE_DEPENDENCIES += jpeg
-else
-SDL2_IMAGE_CONF_OPTS += --disable-jpg
-endif
-
-ifeq ($(BR2_PACKAGE_LIBPNG),y)
-SDL2_IMAGE_CONF_OPTS += --enable-png
-SDL2_IMAGE_DEPENDENCIES += libpng
-else
-SDL2_IMAGE_CONF_OPTS += --disable-png
-endif
-
-ifeq ($(BR2_PACKAGE_TIFF),y)
-SDL2_IMAGE_CONF_OPTS += --enable-tif
-SDL2_IMAGE_DEPENDENCIES += tiff
-else
-SDL2_IMAGE_CONF_OPTS += --disable-tif
-endif
-
-ifeq ($(BR2_PACKAGE_WEBP),y)
-SDL2_IMAGE_CONF_OPTS += --enable-webp
-SDL2_IMAGE_DEPENDENCIES += webp
-else
-SDL2_IMAGE_CONF_OPTS += --disable-webp
-endif
+SDL2_IMAGE_DEPENDENCIES = sdl2 \
+	$(if $(BR2_PACKAGE_SDL2_IMAGE_JPEG),jpeg) \
+	$(if $(BR2_PACKAGE_SDL2_IMAGE_PNG),libpng) \
+	$(if $(BR2_PACKAGE_SDL2_IMAGE_TIFF),tiff) \
+	$(if $(BR2_PACKAGE_SDL2_IMAGE_WEBP),webp)
 
 $(eval $(autotools-package))
