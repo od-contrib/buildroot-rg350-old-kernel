f2788ecea2520b0b31a765ac7f479fe883084696
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-04-05 18:07:24 +0100

    Update localfs and modules init
    Based on OD upstream

--- board/opendingux/gcw0/target_skeleton/etc/init.d/S15localfsinit.sh
+++ board/opendingux/gcw0/target_skeleton/etc/init.d/S15localfsinit.sh
@@ -1,23 +1,14 @@
 #!/bin/sh
 
-if [ -z "$1" ] || [ "x$1" = "xstart" ]; then
-	mkdir -p /media/data/apps
+[ -z "$1" ] || [ "x$1" = "xstart" ] || exit 0
 
-	mkdir -p /media/data/local
-	for i in bin etc home lib sbin share; do
-		mkdir -p /usr/local/$i
-	done
+mkdir -p /media/data/apps /media/data/local/home
 
-	mount -o remount,rw /media
-	ln -s data/local/home /media/home
-	mount -o remount,ro /media
+mount -o remount,ro /media
 
-	if [ ! -f /usr/local/etc/shadow ]; then
-		echo 'root:*:::::::' > /usr/local/etc/shadow
-		chmod 600 /usr/local/etc/shadow
-	fi
+for i in bin etc home lib sbin share; do
+	mkdir -p /usr/local/$i
+done
 
-	if [ ! -f /usr/local/etc/timezone ]; then
-		echo 'CET-1CEST,M3.5.0,M10.5.0' > /usr/local/etc/timezone
-	fi
-fi
+mkdir -p /var/run/sudo /var/tmp /var/log /var/lib
+chmod 777 /var/tmp /var/log
