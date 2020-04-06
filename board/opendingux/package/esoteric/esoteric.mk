e50135a0be410fc6c8dd696decbcca218d931bdd
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-04-07 00:42:11 +0100

    board/opendingux: Cleanup OPK style

--- board/opendingux/package/esoteric/esoteric.mk
+++ board/opendingux/package/esoteric/esoteric.mk
@@ -8,8 +8,8 @@ ESOTERIC_SITE = $(call github,podulator,gmenunx,$(ESOTERIC_VERSION))
 ESOTERIC_DEPENDENCIES = dejavu libpng sdl sdl_ttf sdl_gfx
 ESOTERIC_INSTALL_TARGET = YES
 
-ESOTERIC_MAKE_OPTS = CROSS_COMPILE="$(TARGET_CROSS)"
-    
+ESOTERIC_MAKE_ENV = CROSS_COMPILE="$(TARGET_CROSS)"
+
 ifeq ($(BR2_PACKAGE_LIBOPK),y)
 ESOTERIC_DEPENDENCIES += libopk
 endif
@@ -19,13 +19,13 @@ ESOTERIC_DEPENDENCIES += libxdgmime
 endif
 
 define ESOTERIC_BUILD_CMDS
-	$(MAKE) $(ESOTERIC_MAKE_OPTS) -C $(@D) all dist \
-		-f Makefile.${BR2_PACKAGE_ESOTERIC_PLATFORM}
+	$(TARGET_MAKE_ENV) $(ESOTERIC_MAKE_ENV) $(MAKE) -C $(@D) all dist \
+	  -f Makefile.$(BR2_PACKAGE_ESOTERIC_PLATFORM)
 endef
 
 define ESOTERIC_INSTALL_ESOTERIC
 	cd $(@D) && ./make_opk.sh
-	install -D -m 0644 $(@D)/esoteric.opk $(BINARIES_DIR)/opks/esoteric.opk
+	$(INSTALL) -D -m 0644 $(@D)/esoteric.opk $(BINARIES_DIR)/opks/
 endef
 ESOTERIC_POST_INSTALL_TARGET_HOOKS += ESOTERIC_INSTALL_ESOTERIC
 
