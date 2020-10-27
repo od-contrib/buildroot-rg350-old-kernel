f0846a38c2a01e859ccf08a4818a4f18ee453729
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-10-27 23:11:24 +0000

    Clean up output directory handling
    The `O` buildroot option allows us to customize the output directory

--- board/opendingux/gcw0/build_kernel.sh
+++ board/opendingux/gcw0/build_kernel.sh
@@ -4,17 +4,32 @@
 
 set -e
 
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 <target device>"
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 <target device> [<output dir>]"
 	exit 64
 fi
 
 TARGET_PLATFORM=rg350
 TARGET_DEVICE="$1"
 
+if [ $# -gt 1 ]; then
+	KERNEL_DIR=$2
+else
+	KERNEL_DIR=output-kernel-${TARGET_DEVICE}
+fi
+
 echo 'Building Linux kernel'
 
 set -x
-make "${TARGET_DEVICE}_kernel_only_defconfig" BR2_EXTERNAL=board/opendingux
-make linux
-board/opendingux/gcw0/build-linux-modules-squashfs.sh output/images/
+make "${TARGET_DEVICE}_kernel_only_defconfig" BR2_EXTERNAL=board/opendingux O=$KERNEL_DIR
+make linux O=$KERNEL_DIR
+
+# Build modules.squashfs
+eval $(make -s printvars VARS=LINUX_DIR QUOTED_VARS=YES O=$KERNEL_DIR)
+if [ -f "${LINUX_DIR}/create_modules_fs.sh" ]; then
+  "${LINUX_DIR}/create_modules_fs.sh"
+  cp "${LINUX_DIR}/modules.squashfs" "${KERNEL_DIR}/images/"
+else
+  echo "modules.squashfs not built because ${LINUX_DIR}/create_modules_fs.sh is missing"
+	exit 1
+fi
--- board/opendingux/gcw0/make_initial_image.sh
+++ board/opendingux/gcw0/make_initial_image.sh
@@ -15,21 +15,21 @@ fi
 TARGET_PLATFORM=rg350
 TARGET_DEVICE="$1"
 
+# Buildroot will build to ./output by default but we have multiple builds
+OUTPUT_DIR=output-${TARGET_DEVICE}
+
 # First, build the linux kernel (it uses older gcc)
 KERNEL_DIR=output-kernel-${TARGET_DEVICE}
-if ! [ -f ${KERNEL_DIR}/vmlinuz.bin ]; then
-	board/opendingux/gcw0/build_kernel.sh $TARGET_DEVICE
-	mkdir -p $KERNEL_DIR
-	mv output/images/vmlinuz.bin output/images/modules.squashfs ${KERNEL_DIR}/
-	rm -rf output
+if ! [ -f ${KERNEL_DIR}/images/vmlinuz.bin ]; then
+	board/opendingux/gcw0/build_kernel.sh $TARGET_DEVICE $KERNEL_DIR
 fi
 echo "Will use linux kernel from ./${KERNEL_DIR}"
 
-make "${TARGET_DEVICE}_defconfig" BR2_EXTERNAL=board/opendingux:opks
-make world mininit host-od-imager
+make "${TARGET_DEVICE}_defconfig" BR2_EXTERNAL=board/opendingux:opks O=$OUTPUT_DIR
+make world mininit host-od-imager O=$OUTPUT_DIR
 
 # Data image (OPKs):
-cd output/images
+cd ${OUTPUT_DIR}/images
 mkdir -p od-imager/apps/
 rm -f od-imager/apps/*
 if [ -d ../../dl/od_local_pack/ ]; then
@@ -44,7 +44,7 @@ fi
 
 # System image
 cp mininit-syspart od-imager/
-cp ../../${KERNEL_DIR}/vmlinuz.bin ../../${KERNEL_DIR}/modules.squashfs od-imager/
+cp ../../${KERNEL_DIR}/images/vmlinuz.bin ../../${KERNEL_DIR}/images/modules.squashfs od-imager/
 cp rootfs.squashfs od-imager/
 # Fallbacks are empty as this is the initial image.
 echo -n > od-imager/vmlinuz.bak
@@ -55,7 +55,7 @@ cp "ubiboot/ubiboot-${TARGET_PLATFORM}.bin" od-imager/ubiboot.bin
 cd -
 
 # Assemble partitions and the final image
-cd output/images/od-imager/
+cd ${OUTPUT_DIR}/images/od-imager/
 ./create_mbr.sh
 ./create_system_image.sh
 ./create_data_image.sh
@@ -64,13 +64,13 @@ cd -
 
 echo
 echo 'SD card image created in:'
-echo output/images/od-imager/images/sd_image.bin
+echo ${OUTPUT_DIR}/images/od-imager/images/sd_image.bin
 echo Size:
-du -sh output/images/od-imager/images/sd_image.bin
+du -sh ${OUTPUT_DIR}/images/od-imager/images/sd_image.bin
 
 echo 'Renaming and compressing...'
 NAME="${TARGET_DEVICE}-$(date +'%Y-%m-%d')$(support/scripts/setlocalversion)"
-cp output/images/od-imager/images/sd_image.bin "output/images/${NAME}.bin"
-zip "output/images/${NAME}.zip" "output/images/${NAME}.bin"
-rm "output/images/${NAME}.bin"
-du -sh "output/images/${NAME}.zip"
+cp ${OUTPUT_DIR}/images/od-imager/images/sd_image.bin "${OUTPUT_DIR}/images/${NAME}.bin"
+zip "${OUTPUT_DIR}/images/${NAME}.zip" "${OUTPUT_DIR}/images/${NAME}.bin"
+rm "${OUTPUT_DIR}/images/${NAME}.bin"
+du -sh "${OUTPUT_DIR}/images/${NAME}.zip"
